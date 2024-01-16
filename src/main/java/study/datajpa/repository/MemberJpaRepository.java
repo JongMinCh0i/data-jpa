49a5e6050c93b8d84d9e86e7d35bdd99c69eb8a4
JongMinCh0i <wonderland314592@gmail.com>
2024-01-16 12:08:31 +0900

    순수 JPA 기반 리포지토리 만들기

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 @Transactional
 public class MemberJpaRepository {
@@ -16,6 +19,26 @@ public class MemberJpaRepository {
     public Member save(Member member) {
         em.persist(member);
         return member;
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m From Member m ", Member.class)
+                .getResultList();
+    }
+
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) From Member m", Long.class)
+                .getSingleResult();
     }
 
     public Member find(Long id) {
