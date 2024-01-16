efe4feb1ec4224bef9f8dd4512ac2b582af4067d
JongMinCh0i <wonderland314592@gmail.com>
2024-01-16 12:24:53 +0900

    Spring Data JPA 기반 리포지토리 만들기

---
+++ src/main/java/study/datajpa/repository/TeamJpaRepository.java
@@ -0,0 +1,42 @@
+package study.datajpa.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Team;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TeamJpaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    // create
+    public Team save(Team team) {
+        em.persist(team);
+        return team;
+    }
+
+    // read
+    public List<Team> findAll() {
+        return em.createQuery("select t From Team t", Team.class).getResultList();
+    }
+
+    public Optional<Team> findById(Long id) {
+        Team team = em.find(Team.class, id);
+        return Optional.ofNullable(team);
+    }
+
+    // count
+    public long count() {
+        return em.createQuery("select count(t) From Team t", Long.class).getSingleResult();
+    }
+
+    // delete
+    public void delete(Team team) {
+        em.remove(team);
+    }
+}
--- src/main/java/study/datajpa/repository/TeamRepository.java
+++ src/main/java/study/datajpa/repository/TeamRepository.java
@@ -1,42 +1,7 @@
 package study.datajpa.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Team;
 
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public class TeamRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    // create
-    public Team save(Team team) {
-        em.persist(team);
-        return team;
-    }
-
-    // read
-    public List<Team> findAll() {
-        return em.createQuery("select t From Team t", Team.class).getResultList();
-    }
-
-    public Optional<Team> findById(Long id) {
-        Team team = em.find(Team.class, id);
-        return Optional.ofNullable(team);
-    }
-
-    // count
-    public long count() {
-        return em.createQuery("select count(t) From Team t", Long.class).getSingleResult();
-    }
-
-    // delete
-    public void delete(Team team) {
-        em.remove(team);
-    }
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
