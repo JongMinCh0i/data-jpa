4907dfbdde527aa9fbafd456c21097f133528a5c
JongMinCh0i <wonderland314592@gmail.com>
2024-01-17 18:14:24 +0900

    Web 확장 - 도메인 클래스 컨버터

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -29,7 +29,7 @@ public class Member extends BaseEntity {
     @JoinColumn(name = "team_id")
     private Team team;
 
-    public Member(String usernameww) {
+    public Member(String username) {
         this.username = username;
     }
 
